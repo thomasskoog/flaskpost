4c7f472770ddb4cf6fcf812f1a4237633ff1c85a
Thomas Skoog <thomas.skoog@gmail.com>
2016-04-28 15:00:13 +0200

    general stuff (new db login+pass in db.php NOT included!)

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,22 @@
 var currentPositionMarker,
     mapCenter = new google.maps.LatLng(59.446802000, 18.0719442),
     map,
-    readRange = 30;
+    readRange = 30,
+    infoWindow = new google.maps.InfoWindow({maxWidth: 300}), marker, i;
+/*
+// Hiding shadow and white background div:s (for styling infowindows in SCSS instead)
+google.maps.event.addListener(infoWindow, 'domready', function(){
+    var iwOuter = $('.gm-style-iw');
+    var iwBackground = iwOuter.prev();
+    iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+    iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+    iwOuter.parent().parent().css({left: '115px'});
+    iwBackground.children(':nth-child(1)').attr('style',function(i,s){return s + 'left: 76px !important;'});
+    iwBackground.children(':nth-child(3)').attr('style',function(i,s){return s + 'left: 76px !important;'});
+    iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow' : 'rgba(72,181,233,0.6) 0px 1px 6px'});
+});*/
 
+// When submitting message: post user input value + lat/lng to database, show result, clear input
 $('#submit').click(function(){
     var mess = $.trim($('#msg').val());
     if(mess){
@@ -23,30 +37,27 @@ $('#submit').click(function(){
     }
 });
 
+// Do not send form the ordinary way, stop process
 $('#form').submit(function(){
     return false;
 });
 
+// Clear form when submitted
 function clearInput(){
     $('#form :input').each(function(){
        $(this).val('');
     });
 }
 
-function initMap(){
-    map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 17,
-        center: mapCenter,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+// Set bottle markers on map by looping through all messages in database table
+function setBottles(){
     var bounds = new google.maps.LatLngBounds();
-    $.post('php/bottleAll.php', function(bottleArray) {
+    $.post('php/bottleAll.php', function(bottleArray){
         var bottles = JSON.parse(bottleArray);
         var image = {
-            url: '../images/message-in-a-bottle-w60.png',
+            url: 'images/message-in-a-bottle-w60.png',
             anchor: new google.maps.Point(0, 94)
         };
-        var infoWindow = new google.maps.InfoWindow({maxWidth: 300}), marker, i;
         for (i = 0; i < bottles.length; i++){
             var position = new google.maps.LatLng(parseFloat(bottles[i].lat), parseFloat(bottles[i].lng));
             bounds.extend(position);
@@ -55,11 +66,17 @@ function initMap(){
                 map: map,
                 icon: image
             });
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    infoWindow.setContent(bottles[i].msg);
+            google.maps.event.addListener(marker, 'click', (function(marker, i){
+                return function(){
+                    infoWindow.close();
+                    infoWindow.setContent(
+                        '<div id="iw-container">' +
+                       '<p>' + bottles[i].msg + '</p>'
+                        + '</div>'
+                    );
+                    // Make sure only to display message when within user range
                     var range = google.maps.geometry.spherical.computeDistanceBetween(marker.position, currentPositionMarker.position);
-                    if (range < readRange){
+                    if (range <= readRange){
                         infoWindow.open(map, marker);
                     }
                 }
@@ -69,13 +86,102 @@ function initMap(){
     });
 }
 
+// Pretty self-explanatory stuff down below
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 17,
+        center: mapCenter,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var styles =
+    [
+        {
+            "featureType":"landscape",
+            "elementType":"all",
+            "stylers": [
+                {"hue":"#FFAD00"},
+                {"saturation":50.2},
+                {"lightness":-34.8},
+                {"gamma":1}
+            ]
+        },
+        {
+            "featureType":"landscape.natural.landcover",
+            "elementType":"geometry.fill",
+            "stylers": [
+                {"color":"#cbb42e"},
+                {"visibility":"on"}
+            ]
+        },
+        {
+            "featureType":"poi",
+            "elementType":"all",
+            "stylers":[
+                {"hue":"#FFC300"},
+                {"saturation":54.2},
+                {"lightness":-14.4},
+                {"gamma":1}
+            ]
+        },
+        {
+            "featureType":"road.highway",
+            "elementType":"all",
+            "stylers":[
+                {"hue":"#FFAD00"},
+                {"saturation":-19.8},
+                {"lightness":-1.8},
+                {"gamma":1}
+            ]
+        },
+        {
+            "featureType":"road.arterial",
+            "elementType":"all",
+            "stylers":[
+                {"hue":"#FFAD00"},
+                {"saturation":72.4},
+                {"lightness":-32.6},
+                {"gamma":1}
+            ]
+        },
+        {
+            "featureType":"road.local",
+            "elementType":"all",
+            "stylers":[
+                {"hue":"#FFAD00"},
+                {"saturation":74.4},
+                {"lightness":-18},
+                {"gamma":1}
+            ]
+        },
+        {
+            "featureType":"water",
+            "elementType":"all",
+            "stylers":[
+                {"hue":"#00FFA6"},
+                {"saturation":-63.2},
+                {"lightness":38},
+                {"gamma":1}
+            ]
+        },
+        {
+            "featureType":"water",
+            "elementType":"geometry.fill",
+            "stylers":[
+                {"visibility":"on"},
+                {"color":"#ffe59c"}
+            ]
+        }
+    ];
+    map.setOptions({styles: styles});
+}
+
 function locError(error){
     alert("The current position could not be found!");
 }
 
 function setCurrentPosition(pos){
     var image = {
-        url: '../images/pirateship_w75.png',
+        url: 'images/pirateship_w75.png',
         anchor: new google.maps.Point(37, 100)
     };
     currentPositionMarker = new google.maps.Marker({
@@ -86,8 +192,9 @@ function setCurrentPosition(pos){
         ),
         icon: image,
         title: "Current Position",
-        zIndex: 999
+        zIndex: 9999
     });
+    // Set visible user range as well
     var circle = new google.maps.Circle({
         strokeColor: '#5e98f0',
         strokeOpacity: 0.5,
@@ -102,6 +209,7 @@ function setCurrentPosition(pos){
         pos.coords.latitude,
         pos.coords.longitude
     ));
+    setBottles();
 }
 
 function displayAndWatch(position){
@@ -134,6 +242,7 @@ function initLocationProcedure(){
     }
 }
 
+// Don't run anything until document is ready
 $(document).ready(function(){
     initLocationProcedure();
 });
