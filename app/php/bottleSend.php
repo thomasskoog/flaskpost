30a60a36aacd55888c31b6db2496db315ab5e741
Thomas Skoog <thomas.skoog@gmail.com>
2016-05-08 22:45:37 +0200

    Result box centering.

--- app/php/bottleSend.php
+++ app/php/bottleSend.php
@@ -12,7 +12,7 @@
             $stmt->bindParam(':lat', $lat, PDO::PARAM_STR);
             $stmt->bindParam(':lng', $lng, PDO::PARAM_STR);
             $stmt->execute();
-            echo "You screw the cork in tightly, and throw the bottle into the water. <br>Bon voyage, little one!";
+            echo "You screw the cork in tightly, and throw the bottle into the water. <br><br>Bon voyage, little one!";
         } catch (PDOException $e){
             echo "The bottle breaks against the sharp rocks! <br> What a waste of paper, ink and bottle!<br>" . $e->getMessage();
         }
